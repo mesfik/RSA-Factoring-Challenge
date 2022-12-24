64a5a723eeb1fb72f29f4bce6f99a5ec343292c6
mesfik <fikreamesfin@gmail.com>
2022-12-24 19:22:59 +0000

    factors

---
+++ factors.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def factors(n):
+    return(n)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        exit(1)
+    num = []
+    with open(sys.argv[1], 'r') as file:
+        lines = file.readlines()
+    for i in lines:
+        num.append(int(i))
+    for i in num:
+        j = factors(i)
+        if j != 1:
+            if i % 2 == 0:
+                j = 2
+            else:
+                for j in range(3, (i//2) + 1, 2):
+                    if (i % j == 0):
+                        j = i // j
+                        break
+            print("{} = {:d}*{}".format(i, i//j, j))
